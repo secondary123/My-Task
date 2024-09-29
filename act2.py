0bb522f6718327d33b438ceb4a5f94244dcd35ad
unknown <yawak4226@gmail.com>
2024-09-29 12:45:46 +0800

    First Commit

---
+++ act2.py
@@ -0,0 +1,12 @@
+#define user_data if variable is set to true
+
+username = int(input("Enter Name : "))
+password = int(input("Enter Name : "))
+
+if username >= 100:
+    print*("Bitoskimkim143")
+
+elif password >= 50:
+    print("Kimkimbitos143")
+else:
+    print("Access Denied")
